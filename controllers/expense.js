fc6f7a1b710c6f64827aafccc0bf7e67f170abb9
Jyotiraditya-lal <vaibhavlal0211@gmail.com>
2023-10-02 21:44:25 +0530

    bug fixes

--- controllers/expense.js
+++ controllers/expense.js
@@ -8,23 +8,34 @@ exports.getExpense = (req, res, next) => {
 };
 
 exports.postExpense = async (req,res,next) => {
-    const amount= req.body.amount;
-    const description = req.body.description;
-    const category= req.body.category
-    const Id= req.user.id
-    await Expense.create({
-        amount: amount,
-        description: description,
-        category: category,
-        userId: Id
-    })
-    res.redirect('/expense/addexpense')
+    try{
+        const amount= req.body.amount;
+        const description = req.body.description;
+        const category= req.body.category
+        const Id= req.user.id
+        await Expense.create({
+            amount: amount,
+            description: description,
+            category: category,
+            userId: Id
+        })
+        res.redirect('/expense/addexpense')
+    }catch(err){
+        console.log(err)
+        res.status(500)
+    }
+    
 }
 
 exports.getExpenseData = async (req,res,next)=>{
-    const Id= req.user.id
-    const expenses= await Expense.findAll({where: {userId: Id}})
-    res.status(201).json({allexpenses: expenses})
+    try{
+        const Id= req.user.id
+        const expenses= await Expense.findAll({where: {userId: Id}})
+        res.status(201).json({allexpenses: expenses})
+    }catch(err){
+        console.log(err)
+        res.status(500)
+    }
 }
 
 exports.deleteExpense = async (req,res,next) =>{
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,14 +3,19 @@ const User= require('../models/user')
 
 
 const authenticate = (req,res,next)=>{
-    const token= localStorage.getItem('token')
+   
+    const token = req.headers['authorization']
     const user= jwt.verify(token,'9abr8ytd3554bfndjjw5745bngfj985') // same secret key
+    if(!token){
+        res.status(404).json({remarks: 'token is null or undefined'})
+    }
+
     User.findByPk(user.userId).then(user=>{
         req.user=user
         next()
     }).catch(err=>{
         console.log(err)
-        res.send(404).json({remarks: 'Something went wrong'})
+        res.status(404).json({remarks: 'Something went wrong'})
     })
 }
 
