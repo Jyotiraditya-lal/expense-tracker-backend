8944cae9d5014b5ad8de9e0f776911df168c9fe0
Jyotiraditya-lal <vaibhavlal0211@gmail.com>
2023-10-05 12:46:42 +0530

    bug fixes

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -1,25 +1,51 @@
 const RazorPay= require('razorpay')
+const Order= require('../models/order')
 
 exports.BuyPremium = async (req,res,next) => {
     try{
+        require('dotenv').config();
+        console.log(process.env.RAZORPAY_KEY_ID)
+        //console.log(req.user)
         var rzp= new RazorPay({
             key_id: process.env.RAZORPAY_KEY_ID,
             key_secret: process.env.RAZORPAY_KEY_SECRET
         })
-        const amount= 25.00
-        rzp.orders.create({amount, currency: 'INR'},(err,order)=>{
-            if(err){
-                throw new Error(JSON.stringify(err))
+        const amount= 2500
+        console.log(rzp.orders)
+        rzp.orders.create({ amount, currency: 'INR' }, (err, order) => {
+            if (err) {
+                console.error("Error creating Razorpay order:", err);
+                return res.status(500).json({ error: "Error creating order" });
             }
-            req.user.createOrder({orderid: order.id, status: 'PENDING'}).then(()=>{
-                return res.status(201).json({order, key_id: rzp.key_id})
-            }).catch(err=>{
-                throw new Error(err)
-            })
-        })
+        
+            Order.create({ orderid: order.id, status: 'PENDING' })
+                .then(() => {
+                    return res.status(201).json({ order, key_id: rzp.key_id });
+                })
+                .catch((err) => {
+                    console.error("Error creating order record:", err);
+                    return res.status(500).json({ error: "Error creating order record" });
+                });
+        });
+        
     }catch(err){
         console.log(err)
         res.status(401)
     }
 } 
+
+exports.updatetransactions = async (req,res,next) =>{
+    try{
+        const { payment_id, order_id} = req.body;
+        const order = await Order.findOne({where: {orderid: order_id}})
+        await order.update({payment_id: payment_id, status: 'SUCCESSFUL'})
+        await req.user.update({isPremium: true})
+        res.status(201).json({success: true, remarks: 'transaction successful'})
+
+    }catch(err){
+        console.log(err)
+        res.status(404).json({success: true, remarks: 'transaction failed'})
+
+    }
+}
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,34 @@
-const jwt=require('jsonwebtoken')
-const User= require('../models/user')
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
+const authenticate = (req, res, next) => {
+    const token = req.headers['authorization'];
 
-const authenticate = (req,res,next)=>{
-   
-    const token = req.headers['authorization']
-    const user= jwt.verify(token,'9abr8ytd3554bfndjjw5745bngfj985') // same secret key
-    if(!token){
-        res.status(404).json({remarks: 'token is null or undefined'})
+    if (!token) {
+        return res.status(401).json({ remarks: 'Token is missing' });
     }
 
-    User.findByPk(user.userId).then(user=>{
-        req.user=user
-        next()
-    }).catch(err=>{
-        console.log(err)
-        res.status(404).json({remarks: 'Something went wrong'})
-    })
-}
+    try {
+        const user = jwt.verify(token, '9abr8ytd3554bfndjjw5745bngfj985'); // same secret key
+        User.findByPk(user.userId)
+            .then((user) => {
+                if (!user) {
+                    return res.status(404).json({ remarks: 'User not found' });
+                }
+                req.user = user;
+                console.log(req.user);
+                next();
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json({ remarks: 'Something went wrong' });
+            });
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ remarks: 'Token is invalid' });
+    }
+};
 
-module.exports= {
+module.exports = {
     authenticate
-}
+};
